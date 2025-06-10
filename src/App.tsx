05a8614983936a531d7ffea2bde6fad9811d5cd4
MUmarOfficial <hafiz.mumer03@gmail.com>
2025-06-10 11:37:35 +0500

    6th commit with concept of custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Notes from "./components/Notes";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Note } from "./types/Note";
 import { AppContext } from "./AppContext";
+import useWindowResize from "./hooks/UseWindowResize";
+import useFetch from "./hooks/useFetch";
 
 const darkTheme = createTheme({
   palette: {
@@ -13,55 +15,33 @@ const darkTheme = createTheme({
 });
 
 const App = () => {
-  const [notes, setNotes] = useState<Note[]>([
-    {
-      id: 1,
-      title: "First Note",
-      content: "This is the content of the first note.",
-      starred: false,
-    },
-    {
-      id: 2,
-      title: "Second Note",
-      content: "This is the content of the second note.",
-      starred: true,
-    },
-    {
-      id: 3,
-      title: "Third Note",
-      content: "This is the content of the third note.",
-      starred: false,
-    },
-    {
-      id: 4,
-      title: "Fourth Note",
-      content: "This is the content of the fourth note.",
-      starred: true,
-    },
-    {
-      id: 5,
-      title: "Fifth Note",
-      content: "This is the content of the fifth note.",
-      starred: false,
-    },
-    {
-      id: 6,
-      title: "Sixth Note",
-      content: "This is the content of the sixth note.",
-      starred: true,
-    },
-  ]);
+  const [notes, setNotes] = useState<Note[]>([]);
 
+  const size = useWindowResize();
+  const { data, loading, error } = useFetch<Note[]>(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+
+  useEffect(() => {
+    setNotes(data ? data : []);
+  }, [data]);
   const handleDelete = (id: number) => {
     setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
   };
 
-  const handleStarChange = (id: number, starred: boolean) => {
+  const handleStarChange = (id: number, completed: boolean) => {
     setNotes((prevNotes) =>
-      prevNotes.map((note) => (note.id === id ? { ...note, starred } : note))
+      prevNotes.map((note) =>
+        note.id === id ? { ...note, completed: completed } : note
+      )
     );
   };
-
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
   return (
     <AppContext.Provider
       value={{
@@ -73,7 +53,11 @@ const App = () => {
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <main className="h-full">
-          <Notes />
+          {size.width < 265 ? (
+            <h1>This screen size not supported !</h1>
+          ) : (
+            <Notes />
+          )}
         </main>
       </ThemeProvider>
     </AppContext.Provider>
